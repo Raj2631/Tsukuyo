ff46009785105045d36a786f56fa1d27e3623a1b
Rakshit Pathak <rajatpathak435@gmail.com>
2020-05-06 15:56:04 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// https://api.jikan.moe/v3/top/anime/1/bypopularity
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> {app} </React.StrictMode>,
   document.getElementById('root')
 );
 
